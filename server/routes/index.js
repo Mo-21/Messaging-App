ad40953a388f8eb11639f4442902b0fe4e6a3ea0
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-20 04:53:25 +0300

    Add Routes Protection

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const authorize = require("../middlewares/authorize");
+const authorize_admin = require("../middlewares/authorizeAdmin");
 
 const users_controller = require("../controllers/users");
 
 router.post("/", users_controller.sign_in);
 router.post("/register", users_controller.sign_up);
+
+router.get("/user_details", authorize, users_controller.get_user_details);
+router.get(
+  "/admin_details",
+  [authorize, authorize_admin],
+  users_controller.get_user_details
+);
 
 module.exports = router;
