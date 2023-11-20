26cbc35c96150c086beca6a11f73b10c7ad7ed15
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-20 03:29:54 +0300

    Adding Login and Register API Route Paths

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const users_controller = require("../controllers/users");
+
+router.post("/", users_controller.sign_in);
+router.post("/register", users_controller.sign_up);
 
 module.exports = router;
