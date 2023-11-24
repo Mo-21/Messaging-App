26403148395b181d1da1b92b942dc47086eca75a
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-24 16:40:43 +0300

    Add Login Page With Axios Post Requests

--- clientside/src/App.tsx
+++ clientside/src/App.tsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import Login from "./Login";
+import { UserDetailsProvider } from "./UserDetailsProvider";
+import useAuth from "./hooks/useAuth";
+
+function Navbar() {
+  const { state } = useAuth();
+
+  console.log(state.userDetails.username);
+  const username = state.userDetails.username || "";
+
+  return (
+    <div className="navbar">
+      <div className="username">{username}</div>
+      <div className="brand">Send</div>
+    </div>
+  );
+}
 
 function App() {
-  return <></>;
+  return (
+    <UserDetailsProvider>
+      <Navbar />
+      <Login />
+    </UserDetailsProvider>
+  );
 }
 
 export default App;
