d2aeeeeeefdf81bffcf5e43513c2f72088343753
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-25 15:07:48 +0300

    Fix undefined value for _id in PrivateRoutes component

--- clientside/src/PrivateRoutes.tsx
+++ clientside/src/PrivateRoutes.tsx
@@ -4,9 +4,8 @@ import { useAuth } from "./useAuth";
 export default function PrivateRoutes() {
   const { state } = useAuth();
 
-  const _id = state.userDetails._id || "";
+  const _id = state.userDetails._id || undefined;
 
-  console.log(_id);
   if (!_id) return <Navigate to={"/login"} />;
   return <Outlet />;
 }
