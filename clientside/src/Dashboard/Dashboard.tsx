0c40ab9b60026a3e95e0c4f77de4de3f646da8ad
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-25 10:03:47 +0300

    Add Dashboard component and related files

---
+++ clientside/src/Dashboard/Dashboard.tsx
@@ -0,0 +1,25 @@
+import "./Dashboard.css";
+import { ChatPartner, useChatPartners } from "./useChatPartners";
+
+export default function Dashboard() {
+  const { data: chatPartners, error, isLoading } = useChatPartners();
+
+  if (error) return <h1>Something went wrong</h1>;
+  if (isLoading) return <h1>Loading...</h1>;
+  
+  return (
+    <div className="dashboard">
+      <div className="contact-list">
+        <span className="contact-list-title">Contacts</span>
+        {chatPartners?.map((chatPartner: ChatPartner) => (
+          <div key={chatPartner.id} className="contact">
+            {chatPartner.username}
+          </div>
+        ))}
+      </div>
+      <div className=" empty-space">
+        Select a chat to start chatting<div>As simple as that</div>
+      </div>
+    </div>
+  );
+}
