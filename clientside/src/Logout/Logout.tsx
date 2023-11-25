91a6e11f57cbad5fbc142a138bf9410e5b8b23f7
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-25 14:12:33 +0300

    Add Dispatch Logout Action To The Reducer

--- clientside/src/Logout/Logout.tsx
+++ clientside/src/Logout/Logout.tsx
@@ -1,16 +1,19 @@
 import { useQueryClient } from "@tanstack/react-query";
 import ClientAPI from "../react-query/services/api-client";
+import { useAuth } from "../useAuth";
 
 const Client = new ClientAPI("/logout");
 
 export default function Logout() {
   const queryClient = useQueryClient();
+  const { dispatch } = useAuth();
 
   const handleLogout = async () => {
     try {
       await Client.logout();
       queryClient.clear(); // Clear the cache
       window.location.reload();
+      dispatch({ type: "LOGOUT" });
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
       throw new Error(err.response.data.message);
